564d34a2d0d8e3e5d4dc26eb55363cb96bc141d1
Andrew Blackburn <spacemanspiff982@gmail.com>
2022-03-09 14:25:17 -0800

    good luck

---
+++ Source/TDTDShooter/TDTDShooter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TDTDShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TDTDShooter, "TDTDShooter" );
