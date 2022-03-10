7ab5981c15dbc855fc1896149c4c64206501d4d1
Andrew Blackburn <spacemanspiff982@gmail.com>
2022-03-09 19:37:27 -0800

    try

---
+++ Source/TDTDShooter/TDTDShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TDTDShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TDTDSHOOTER_API ATDTDShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
